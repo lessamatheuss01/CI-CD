0a97429df9173aed5ae3a9a3cb1457eba2239de1
Matheus Lessa <matheus.lessa@sptech.school>
2024-05-28 19:37:52 -0300

    Adding.tests

---
+++ src/test/java/sptech/school/exemplo/actions/service/CalculadoraServiceTest.java
@@ -0,0 +1,21 @@
+package sptech.school.exemplo.actions.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraServiceTest {
+    @Test
+    @DisplayName("Quando aciona com 10 e 2, então deve retornar 5")
+    public void testDividir(){
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 9;
+        double esperado = 5;
+
+        double resultado = calculadoraService.dividir(a, b);
+
+        assertEquals(esperado, resultado);
+    }
+}
